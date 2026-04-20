d47a268b79b35ed189161164c40c1532604c7fbc
Pau Montalva <paumontalva46@alumnos.cei.es>
2026-04-20 22:08:09 +0200

    Web final con imagenes arregladas

--- js/index.js
+++ js/index.js
@@ -33,5 +33,44 @@ function canviarImatge() {
     imatgeElement.src = `carrusel/${imatges[index]}.jpg`;
 }
 
+
 // 5. Por último le digo cada cuanto tiene que hacer el cambio.
 setInterval(canviarImatge, 3000);
+
+
+
+
+ //IMAGEN ETIQUETA PARA CADA PROJECT
+
+
+ //Primero selecciono los enlaces que estan dentro del project item
+
+document.addEventListener("DOMContentLoaded", () => {
+   
+    const links = document.querySelectorAll('.Project-item a');
+    
+    // Despues selecciono el conteedor que hará de fondo
+    const contenedor = document.querySelector('.Main-works');
+
+    links.forEach(link => {
+        link.addEventListener('mouseenter', () => {
+            //le aplicamos un addeventlistener a cada link y cogemos la ruta que hemos puesto antes , en este caso me ayudé de la ia
+            const rutaImagen = link.getAttribute('data-img');
+            
+            console.log("Canviant fons a: " + rutaImagen); // Para poder verlo en la consola
+            
+            if (rutaImagen) {
+                contenedor.style.backgroundImage = `url('${rutaImagen}')`;
+                contenedor.style.backgroundSize = 'cover';
+                contenedor.style.backgroundPosition = 'center';
+                contenedor.style.backgroundAttachment = 'fixed'; 
+            }
+        });
+                  
+        //Aqui me ayude de la ia porque queria hacer que cuando no estuviera el raton encima no huviera ninguna foto
+        link.addEventListener('mouseleave', () => {
+            
+            contenedor.style.backgroundImage = 'none';
+        });
+    });
+});
